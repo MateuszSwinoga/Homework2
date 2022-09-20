b1e285651ba4ffb4b7ccf8b7b313aeb6f67666c6
Mateusz Świnoga <mateusz19111@gmail.com>
2022-09-21 01:03:28 +0200

    enter checking with add happy shot list

--- Homework2.java
+++ Homework2.java
@@ -1,7 +1,6 @@
-import javax.swing.*;
 import java.util.Scanner;
 
-public class Main {
+public class Homework2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give me your name");
@@ -15,32 +14,29 @@ public class Main {
 
 
 
+
+
         if (age>=18) {
             System.out.println(name + " " + surname + " " + "You can enter");
             if (age>=18&25>=age) {
+                System.out.println("You have free beer for your age");
                 for(String person:happyList)
-                if (name == person){
-                    System.out.println("Congratulation! Your name is on the happy list. You have also free shot!");
-
-                }
-                else{
-                    System.out.println("You have free beer for your age");
-
-
-
-                }
+                    if (name.equals(person)) {
+                System.out.println("Congratulation! Your name is on the happy list. You have also free shot!");
+                break;
+            }
             }
                 else {
                     System.out.println("You're to old for free beer");
                 }
 
         }
-
         else{
             System.out.println(name + " " + surname + " " + "You can't enter");
-
-
-
         }
+
+
+
+
     }
 }
